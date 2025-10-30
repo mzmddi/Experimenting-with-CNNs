87a382f483299a8e481fd99a634e5044c6d221ad
mzmddi <110358757+mzmddi@users.noreply.github.com>
2025-10-30 15:08:24 -0400

    added the venv creation in the notes section

--- cnnExperimenting.py
+++ cnnExperimenting.py
@@ -1,4 +1,15 @@
 # ---NOTES---
+"""
+./cnnExperimenting.py
+entry-way to perform the experimentations
+
+// Setting up the virtual environment
+    ** at the root directory **
+    (1) python3 -m venv .venv
+    (2) source .venv/bin/activate
+    ** if (some or all) requirements are not installed yet **
+    (3) pip3 install -r requirements.txt
+"""
 
 # ---IMPORTS---
 import sys
